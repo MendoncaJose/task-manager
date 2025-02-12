539b53f44e24b2b48b6302dad05e2fb36ba50233
José Mendonça <jose.mendonca@cyango.com>
2025-02-12 12:31:07 +0000

    taskList-fe-19: (tasks.module.ts) Modularized task-related components

---
+++ src/app/pages/task.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TasksListComponent } from './task-list/task-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskListRoutingModule } from './task-routing.module';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import {
+  TaskStatusColorDirective,
+  TaskStatusPipe,
+} from '../pipes/task-status.pipe';
+
+@NgModule({
+  declarations: [
+    TasksListComponent,
+    TaskFormComponent,
+    TaskDetailComponent,
+    TaskStatusPipe,
+    TaskStatusColorDirective,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TaskListRoutingModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    TasksListComponent,
+    TaskFormComponent,
+    TaskDetailComponent,
+    TaskStatusPipe,
+    TaskStatusColorDirective,
+  ],
+})
+export class TaskModule {}
