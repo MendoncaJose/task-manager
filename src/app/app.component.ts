495b25915ea3be94262106606b3b612fec0d9a63
José Mendonça <jose.mendonca@cyango.com>
2025-02-12 17:21:20 +0000

    taskList-fe-29: (app.component.ts) Updated main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',  // O seletor usado no HTML, como <app-root></app-root>
-  templateUrl: './app.component.html',  // Referência para o template HTML
-  styleUrls: ['./app.component.scss']  // Arquivo CSS ou SCSS (se necessário)
+  selector: 'app-root', // O seletor usado no HTML, como <app-root></app-root>
+  templateUrl: './app.component.html', // Referência para o template HTML
+  styleUrls: ['./app.component.scss'], // Arquivo CSS ou SCSS (se necessário)
 })
 export class AppComponent {
-  title = 'test-app';  // Variável de exemplo, você pode alterar conforme necessário
+  title = 'test-app'; // Variável de exemplo, você pode alterar conforme necessário
 }
