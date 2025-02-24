53636012f702bbcd0085cef63a1916b2d2595ed4
José Mendonça <jose.mendonca@cyango.com>
2025-02-24 15:14:23 +0000

    taskList-fe-74(languange-selector.component.ts) fix

--- src/app/components/language-selector/language-selector.component.ts
+++ src/app/components/language-selector/language-selector.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { MatSelectModule } from '@angular/material/select';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-language-selector',
-  standalone: true,
-  imports: [CommonModule, MatSelectModule, FormsModule],
   templateUrl: './language-selector.component.html',
   styleUrls: ['./language-selector.component.scss'],
 })
